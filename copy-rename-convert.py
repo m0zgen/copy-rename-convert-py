9332ac7b13ec038cdb9f2a04578e400b8d46af23
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-10-05 10:41:42 +0600

    Create functions. Format and optimize code

--- copy-rename-convert.py
+++ copy-rename-convert.py
@@ -2,54 +2,70 @@
 
 import os, shutil, string
 
+# Path to converted images and videos
 path = "/mnt/data/tt/collect-test/pc/"
+
+# Path to original source images
+sourcedir = path + "or"
+
 extentions = (".jpg", ".jpeg", ".JPG")
 
-# Delete all jpg in path folder
-filelist = [ f for f in os.listdir(path) if f.endswith(".jpg")]
-for f in filelist:
-    os.remove(path + f)
-    print "Remove: " + f
-
-# Delete all JPG in path folder
-filelist = [ f for f in os.listdir(path) if f.endswith(".JPG")]
-for f in filelist:
-    os.remove(path + f)
-    print "Remove: " + f
-
-# Sort file names with path
-file_list = os.listdir(path + "or")
-full_list = [os.path.join(path + "or", i) for i in file_list]
+# Sort file names with path contains original images
+file_list = os.listdir(sourcedir)
+full_list = [os.path.join(sourcedir, i) for i in file_list]
 time_sorted_list = sorted(full_list, key = os.path.getmtime)
 # print time_sorted_list
 
-# Only file names
+# Sort only file names (without path to file)
 sorted_filename_list = [os.path.basename(i) for i in time_sorted_list]
 # print sorted_filename_list
 
-# Rename files and copy
-sourcedir = path + "or"
-number_digits = 5
-extentions = (".jpg", ".jpeg", ".JPG")
-count = 0
+# Remove renamed files from "path" folder
+def removeFiles():
 
-for item in sorted_filename_list:
-    if item.endswith(extentions):
-      name = item.split(".")
-      count = count + 1
+  # Delete all jpg / JPG in path folder
+  files = os.listdir(path)
+  for file in files:
+    if file.endswith(extentions):
+      os.remove(path + file)
+      print "Remove: " + file
 
-      n = "_" + string.zfill(count,5) + ".jpg"
-      # newname = item + n
-      newname = "img" + n
-      print newname
 
-      # shutil.copy(sourcedir + "/" + item, path + newname)
+def renameAndConvert():
 
-      # Run bash
-      # convert = "convert " + path + newname + " -resize 1920x1060 " + path + newname
-      # print convert
-      # os.system(convert)
-      #
+  # Counting
+  number_digits = 5
+  count = 0
+
+  # Rename files and copy to "path"
+  for item in sorted_filename_list:
+      if item.endswith(extentions):
+        name = item.split(".")
+        count = count + 1
+
+        n = "_" + string.zfill(count,5) + ".jpg"
+        # newname = item + n
+        newname = "img" + n
+        print newname
+
+        shutil.copy(sourcedir + "/" + item, path + newname)
+
+        # Run bash
+        # convert = "convert " + path + newname + " -resize 1920x1060 " + path + newname
+        # print convert
+        # os.system(convert)
+        #
+
+removeFiles()
+# renameAndConvert()
+
+
+
+
+
+
+
+
 
 
 
